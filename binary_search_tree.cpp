5c1f8cec9d77dcfdac7e0c398bb99f4015243b86
CSCIMattAustin <matgaust@ut.utm.edu>
2016-11-06 15:32:32 -0600

    got find, set, and print working

--- binary_search_tree.cpp
+++ binary_search_tree.cpp
@@ -11,61 +11,63 @@ void binary_search_tree::set(string key, int data){
   Node *insert_node = new Node;
   insert_node->data = data;
   insert_node->key = key;
-  if (root == NULL)
+  if (root == NULL)//insert at front
     root = insert_node;
-  else if(find(key) == data){
+  else if(find(key) != -1){//if the key is already in the tree
     Node *traverse = root;
-    while(traverse -> key != key){
+    while (traverse -> key != key){
       if(traverse ->left->key > key)
-	traverse = traverse ->left;
+  	traverse = traverse ->left;
       else if(traverse->right->key < key)
-	traverse = traverse->right;
+  	traverse = traverse->right;
     }
     traverse->data = data;
-    }
-  else
+  }
+  else//search for the correct place to insert
     set_helper(root, key, data);
 
     }
 void binary_search_tree::set_helper(Node *current, string key, int data){
   if(current != NULL){
-    if(current -> key > key){
-      if(current ->left != NULL)
-	set_helper(current->left, key, data);
-      else {
+  if(current -> key > key){
+    if(current ->left != NULL)
+      set_helper(current->left, key, data);
+    else {
       Node *insert = new Node;
-	insert->data = data;
-	insert->key = key;
-	current->left = insert;
-      }
+      insert->data = data;
+      insert->key = key;
+      current->left = insert;
     }
-    else if( current->key < key){
-      if(current ->right != NULL)
-	set_helper(current->right, key, data);
-      else{
-	Node *insert = new Node;
-	insert->data = data;
-	insert->key = key;
-	current->right = insert;
-      }
+  }   
+  else if( current->key < key){
+    if(current ->right != NULL)
+      set_helper(current->right, key, data);
+    else{
+      Node *insert = new Node;
+      insert->data = data;
+      insert->key = key;
+      current->right = insert;
     }
   }
+  }
 }
+
 int binary_search_tree::find(string key){
   if(root == NULL)
     return -1;
-  else if(root -> key == key)
-    return root->data;
-  else {
-    Node *traverse = root;
-    while(traverse -> key != key){
-      if(traverse ->left->key > key)
-	traverse = traverse ->left;
-      else if(traverse->right->key < key)
-	traverse = traverse->right;
-    }
-    return traverse->data;
+  else
+    return find_helper(root, key);
+}
+int binary_search_tree::find_helper(Node *current, string key){
+  if (current -> key != key){  
+    if (current -> left != NULL && current->left->key > key)
+      return find_helper(current ->left, key);
+    else if (current -> right != NULL && current->right->key < key)
+      return find_helper(current ->right, key);
+    else
+      return -1;
   }
+  return current -> data;
 }
 void binary_search_tree::print(){
   if(root != NULL)
