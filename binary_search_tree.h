b2ea6c1455b4eb8e34cc9863dcb5e65452c20b71
CSCIMattAustin <matgaust@ut.utm.edu>
2016-11-17 12:24:22 -0600

    everything is working

--- binary_search_tree.h
+++ binary_search_tree.h
@@ -1,53 +1,59 @@
+/*Author: Matthew Austin
+ *File: binary_search_tree.h
+ *Date:11/15/2016
+ *Purpose:This is the header file for the binary_search_tree class. 
+ */
 #ifndef BINARY_SEARCH_TREE_H
 #define BINARY_SEARCH_TREE_H
 
 #include <iostream>
 #include <string>
-#include <iomanip>
 #include <fstream>
 using namespace std;
 
-class Node{
+
+class Node{//Node class
  public:
-  Node *left, *right;
-  string key;
-  int data;
-  Node(string key, int data){
+  Node *left, *right;//pointers to the next two Nodes
+  string key;//key to find a specific Node
+  int data;//the data that is associated with the key
+  Node(string key, int data){//makes setting new nodes easier
     left = NULL;
-    right = NULL;
-    this->key = key;
-    this->data = data;
+    right = NULL;//initializing the pointers
+    this->key = key;//sets the key
+    this->data = data;//sets the data
   }
 
 };
 
 class binary_search_tree {
  private:
-  Node *root;
-  void destructor_helper(Node *current);
-  void set_helper(Node *current, string key, int value);
-  int find_helper(Node *current, string key);
-  string min_helper(Node *current);
-  string max_helper(Node *current);
-  void read_helper(Node *current, string key);
-  void print_helper(Node *current);
-  void remove_bad_char(string &key);
-  void remove_helper(Node *&current, string key);
-  void save_helper(Node *current, ofstream &oFile);
+  Node *root;//pointer to the root of the tree
+  void destructor_helper(Node *current);//helps the destructor and erase_tree to allow reucursion
+  void set_helper(Node *current, string key);//helps the set function to allow recursion
+  int find_helper(Node *current, string key);//helps the find function to allw recursion
+  string min_helper(Node *current);//helper for min to allow recursion
+  string max_helper(Node *current);//helper for max to allow recursion
+  void read_helper(Node *current, string key);//helper for the read function to set the items from the file
+  void read_find(Node *current, string key);//finds the key and increments the data if it is in there
+  void remove_bad_char(string &key);//removes characters the unwanted characters to get an accurate count of the words in the file
+  void print_helper(Node *current);//recursively prints the tree
+  void remove_helper(Node *&current, string key);//helps remove by finding the Node to remove based on the key
+  void save_helper(Node *current, ofstream &oFile);//Helpf the save function write to the given file
+ int& operator_helper(Node *&current, string key);
  public:
-  binary_search_tree();
-  ~binary_search_tree();  
-  void set(string key, int data);
-  int find(string key);
-  void print();  
-  void min();
-  void max();  
-  void read_from_file(string file);
-  void read_find(Node *current, string key);   
-  void save_file(string file);
-  void remove(string key);
-  void erase_tree();
-  int operator[](string key);
+  binary_search_tree();//sets the root to NULL
+  ~binary_search_tree();//destructs the tree when the program ends
+  void set(string key);//sets the given key and data in the tree
+  int find(string key);//return the data that is associates with the key
+  void print();//prints the tree in lexicographical order
+  void min();//prints the first key lexicographically
+  void max();//prints the last key lexicographically
+  void read_from_file(string file);//reads from a file and inserts into the tree
+  void save_file(string file);//Writes the tree to a given file
+  void remove(string key);//removes a specific Node and rearranges the Nodes if necessary
+  void erase_tree();//erases the entire tree
+  int& operator[](string key);//overloads the []operator to return the data
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,68 +1,78 @@
-
-#include <iostream>
+/*Author: Matthew Austin
+ *File: main.cpp
+ *Date:11/15/2016
+ *Purpose: This is the main driver for my program that allows manipulation of 
+ *the tree
+ */
 #include "binary_search_tree.h"
-
 using namespace std;
+
 void print_welcome();
+
 int main(){
   string file;
   binary_search_tree first_tree;
   string key;
-  int data;
   char answer;
-  print_welcome();
-  cout << "\n?";
-  cin >> answer;
+  print_welcome();//welcome screen
+  cin >> answer;//priming read
   while(answer != 'q'){
-    if (answer == '1'){
-    cout << "Please enter the key you would like to insert: ";
-    cin >> key;
-    cout << "Please enter the data you would like " 
-	 << "to be associated with the key: ";
-    cin >> data;
-    first_tree.set(key, data);
-}
-    else if(answer == '2')
+    if (answer == '1'){//inserts a given key and data
+      cout << "Please enter the key you would like to insert: ";
+      cin >> key;
+      if (first_tree.find(key) != -1){
+	
+	first_tree[key]++;
+	cout << "The count for [" << key << "] is " << first_tree[key] << endl;
+      }
+      else {
+	first_tree.set(key);
+	cout << "The count for [" << key << "] is " << first_tree[key] << endl;
+      }
+    }
+    else if(answer == '2')//prints the tree lexicographically
       first_tree.print();
-    else if(answer == '3')
+    else if(answer == '3')//prints the first key lexicographically
       first_tree.min();
-    else if(answer == '4')
+    else if(answer == '4')//prints the last key lexicographically
       first_tree.max();
-    else if(answer == '5'){
+    else if(answer == '5'){//reads from a file and inserts into the tree
       cout << "Please enter a file to read from: ";
       cin >> file;
       first_tree.read_from_file(file);
     }
-    else if (answer == '6'){
+    else if (answer == '6'){//removes a specific Node basedon the key
       cout << "Please enter the key of the item you wish to delete: ";
       cin >> key;
       first_tree.remove(key);
     }
-    else if (answer == '7'){
+    else if (answer == '7'){//saves the tree to a given file
       cout << "Please enter the file you wish to save the tree in: ";
       cin >> file;
       first_tree.save_file(file);
     }
-    else if (answer == '8'){
+    else if (answer == '8'){//find the data for a given key
       cout << "Please enter the key of the data you wish to know: ";
       cin >> key;
-      cout << "The data for " << key << " is " << first_tree[key] << endl;
+      cout << "The count for " << key << " is " << first_tree[key] << endl;
     }
-    else if (answer == '9')
+    else if (answer == '9')//erase the entire tree
       first_tree.erase_tree();
-      print_welcome();
-    cout << "\n?";
-    cin >> answer;
-}
+    print_welcome();//welcome screen
+    cin >> answer;//last read
+  }
   return 0;
 }
 void print_welcome(){
   for (int i = 0; i<54; i++)
-    cout << "-";
+    cout << "-";//top of the box
+  //Welcome screen
   cout << "\n|<<<<<WELCOME TO THE FANTASTIC TREE MANIPULATOR>>>>>>|"
        << "\n|<<<<<Please enter one of the following options>>>>>>|\n|";
-  for(int i = 0; i<52; i++)
+  
+  for(int i = 0; i<52; i++)//empty line
     cout << " ";
+  //options
   cout << "|\n|<<(1)->Enter an item to be inserted into the tree>>>|"
        << "\n|<<(2)->Print the tree>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>|"
        << "\n|<<(3)->Print the first word alphabetically>>>>>>>>>>|"
@@ -70,10 +80,11 @@ void print_welcome(){
        << "\n|<<(5)->Read from a file>>>>>>>>>>>>>>>>>>>>>>>>>>>>>|"
        << "\n|<<(6)->Remove an item>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>|"
        << "\n|<<(7)->Save tree to a file>>>>>>>>>>>>>>>>>>>>>>>>>>|"
-       << "\n|<<(8)->Get the data from a given key>>>>>>>>>>>>>>>>|"
+       << "\n|<<(8)->Get the count of a given key>>>>>>>>>>>>>>>>>|"
        << "\n|<<(9)->Erase the entire tree>>>>>>>>>>>>>>>>>>>>>>>>|"
        << "\n|<<(q)->Quit>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>|"
        << endl;
   for (int i = 0; i<54; i++)
-    cout << "-";
+    cout << "-";//bottom of the box
+  cout << "\n?";
 }
