980d0c2b362f14934e25ee9dc9cf7ab3b71abc1f
CSCIMattAustin <matgaust@ut.utm.edu>
2016-11-17 23:51:48 -0600

    complete

--- binary_search_tree.h
+++ binary_search_tree.h
@@ -29,30 +29,146 @@ class Node{//Node class
 class binary_search_tree {
  private://These don't need to be accessed directly in main
   Node *root;//pointer to the root of the tree
-  void destructor_helper(Node *current);//helps the destructor and erase_tree to allow reucursion
-  void set_helper(Node *current, string key);//helps the set function to allow recursion
-  int find_helper(Node *current, string key);//helps the find function to allw recursion
-  string min_helper(Node *current);//helper for min to allow recursion
-  string max_helper(Node *current);//helper for max to allow recursion
-  void read_helper(Node *current, string key);//helper for the read function to set the items from the file
-  void read_find(Node *current, string key);//finds the key and increments the data if it is in there
-  void remove_bad_char(string &key);//removes characters the unwanted characters to get an accurate count of the words in the file
-  void print_helper(Node *current);//recursively prints the tree
-  void remove_helper(Node *&current, string key);//helps remove by finding the Node to remove based on the key
-  void save_helper(Node *current, ofstream &oFile);//Helpf the save function write to the given file
-  int& operator_helper(Node *&current, string key);//operator overload helper function
+  void destructor_helper(Node *current);
+  //helps the destructor and erase_tree to allow reucursion
+  //Pre-conditions:
+  //    There is something in the tree
+  //Post-conditions:
+  //    There is nothing in the tree
+  //
+  void set_helper(Node *current, string key);
+  ////helps the set function to allow recursion
+  //Pre-conditions:
+  //   There is something in the tree
+  //Post-conditions:
+  //   Sets the passed in key in the apropriate place
+  //       
+  int find_helper(Node *current, string key);
+  //helper function for find returns the data associated with the key
+  //Pre-conditions:
+  //There is something in the tree
+  //       
+  string min_helper(Node *current);
+  //helper for min to allow recursion
+  //Pre-conditions:
+  //There is someething in the tree
+  //       
+  string max_helper(Node *current);
+  ////helper for max to allow recursion
+  //Pre-conditions:
+  //There is something in the tree
+  //       
+  void read_helper(Node *current, string key);
+  // reads in a file and inserts in the tree in sorted order
+  //Pre-conditions:
+  //There is something in the tree
+  //Post-conditions:
+  //There is now a tree filled with the strings from the file
+  //       
+  void read_find(Node *current, string key);
+  //finds the key and increments the data if it is in there
+  //Pre-conditions:
+  //There is something in the tree and the key is in the tree
+  //Post-conditions:
+  //the correct data is incremented
+  //       
+  void remove_bad_char(string &key);
+  //removes characters the unwanted characters to get an accurate count of the words in the file
+  //Pre-conditions:
+  //the string is a valid string
+  //Post-conditions:
+  //the key no longer has unwanted characters
+  //       
+  void print_helper(Node *current);
+  //recursively prints the tree
+  //Pre-conditions:
+  //There is something in the tree
+  //       
+  void remove_helper(Node *&current, string key);
+  //helps remove by finding the Node to remove based on the key
+  //Pre-conditions:
+  //There is something in the tree
+  //Post-conditions:
+  //The Node with key is removed
+  //       
+  void save_helper(Node *current, ofstream &oFile);
+  //Helps the save function write to the given file
+  //Pre-conditions:
+  //There is something in the tree
+  //Post-conditions:
+  //the tree i written to a file
+  //       
+  int& operator_helper(Node *&current, string key);
+  //operator overload helper function
+  //Pre-conditions:
+  //THere is something in the tree
+  //Post-conditions:
+  //returns a reference to an integer that is associated with key
+  //       
  public:
-  binary_search_tree();//sets the root to NULL
-  ~binary_search_tree();//destructs the tree when the program ends
-  void set(string key);//sets the given key and data in the tree
-  int find(string key);//return the data that is associates with the key
-  void print();//prints the tree in lexicographical order
-  void min();//prints the first key lexicographically
-  void max();//prints the last key lexicographically
-  void read_from_file(string file);//reads from a file and inserts into the tree
-  void save_file(string file);//Writes the tree to a given file
-  void remove(string key);//removes a specific Node and rearranges the Nodes if necessary
-  void erase_tree();//erases the entire tree
-  int& operator[](string key);//overloads the []operator to return the data
+  binary_search_tree();
+  //sets the root to NULL for an empty tree
+  //       
+  ~binary_search_tree();
+  //destructs the tree when the program ends
+  //Post-conditions:
+  //The tree is destructed and the program is terminated
+  //       
+  void set(string key);
+  //sets the given key and data in the tree
+  //Post-conditions:
+  //The passed in key is inserted into the tree lexicographically
+  //       
+  int find(string key);
+  //return the data that is associates with the key
+  //Post-conditions:
+  //returns the data associated with key
+  //       
+  void print();
+  //prints the tree in lexicographical order
+  //Post-conditions:
+  //the tree is printed lexicographically
+  //       
+  void min();
+  //prints the first key lexicographically
+  //       
+  void max();
+  //prints the last key lexicographically
+  //       
+  void read_from_file(string file);
+  //reads from a file and inserts into the tree
+  //Post-conditions:
+  //The strings from the file are inserted
+  //       
+  void save_file(string file);
+  //Writes the tree to a given file
+  //Pre-conditions:
+  //There is something in the tree
+  //Post-conditions:
+  //The tree is written to a file
+  //       
+  void remove(string key);
+  //removes a specific Node and rearranges the Nodes if necessary
+  //Pre-conditions:
+  //There is something in the tree
+  //Post-conditions:
+  //The given key is removed
+  //       
+  void erase_tree();
+  //erases the entire tree
+  //Pre-conditions:
+  //There is something in the tree
+  //Post-conditions:
+  //The tree is deallocated
+  //       
+  int& operator[](string key);
+  //overloads the []operator to return the data
+  //Pre-conditions:
+  //The key is in the tree
+  //Post-conditions:
+  //return a reference to the integer in the Node
+  //       
+  //       
+  //       
 };
 #endif //BINARY_SEARCH_TREE_H
